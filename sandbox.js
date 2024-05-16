5d93101fc4f44b73cc0b4ea54d9057314f57fbf5
Sandip <sandip.bagale34@gmail.com>
2024-05-16 13:30:55 +0545

    Lesson 2

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var character = 'luiza';
+console.log(character);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
