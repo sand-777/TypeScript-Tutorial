31c9d216f67dac646906b766d0bde258cc8bbc82
Sandip <sandip.bagale34@gmail.com>
2024-05-17 12:52:18 +0545

    lesson 8

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log('mrio');
+let greet;
+// greet = "hello"
+greet = () => {
+    console.log("hello,again");
+};
+const add = (a, b, c) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, '20');
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,14 @@
-console.log('mrio')
+let greet : Function;
+
+// greet = "hello"
+
+greet = ()=>{
+    console.log("hello,again");
+}
+
+const add = (a:number, b: number,c?:number | string)=>{
+console.log(a + b);
+console.log(c)
+}
+
+add(5,10,'20');
