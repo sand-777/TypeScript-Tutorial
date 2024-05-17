23cf5e7b17df697a4425b5baee4e118f704a4001
Sandip <sandip.bagale34@gmail.com>
2024-05-17 15:08:47 +0545

    lesson 12

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,37 @@
-// const anchor = document.querySelector('a')!;
 
-// if(anchor){
-//     console.log(anchor.href);
-// }
+//classes
+class Invoice {
+    client: string;
+    details: string;
+    amount : number;
 
-// console.log(anchor.href);
+    constructor(c: string,d : string,a : number){
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
 
-// const form = document.querySelector('form')!;
+    format(){
+        return `${this.client} owes £${this.amount} for ${this.details}`
+    }
+}
+
+const invOne = new Invoice("mario","work on the mario website",250);
+const invTwo = new Invoice("luigi","work on the luigi website",300);
+
+// console.log(invOne,invTwo)
+
+let invoices: Invoice[] = [];
+invoices.push(invOne)
+invoices.push(invTwo)
+
+// invOne.client = 'yoshi';
+// invTwo.amount = 400;
+
+console.log(invOne,invTwo);
+
+console.log(invoices)
+
 
 const form = document.querySelector('.new-item-form')! as HTMLFormElement;
 
